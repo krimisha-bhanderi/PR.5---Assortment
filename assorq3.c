66499c1489d58a7328482f01a3fed430edb3777e
krimisha-bhanderi <krimisha025@gmail.com>
2023-12-11 13:48:40 +0000

    program

---
+++ assorq3.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+void main(){
+	
+	int i,j, row,column, swap;
+
+	 printf("Enter size of row:- ");
+	 scanf("%d",&row);
+	 printf("Enter size of column:- ");
+	 scanf("%d",&column);
+	 
+	 	int a[row][column];
+	 	
+	for(i=0; i<row; i++){
+	for(j=0; j<column; j++){
+	
+		printf("Enter a[%d][%d]:- ",i,j);
+		scanf("%d",& a[i][j]);
+		}
+	}
+		for(i=0; i<row; i++){
+			for(j=0; j<column; j++){
+				
+				printf("%d ",a[i][j]);
+		
+	    	}
+	    	printf("\n");
+		}
+		printf("\n");
+		
+		printf("Transpose matrix is:\n");
+	for(i=0; i<row; i++){
+		for(j=0; j<column; j++){
+			
+			printf("%d ",a[j][i]);
+			
+		}
+	    	printf("\n");
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
